883e5ebd76a030fb32630cf19cde72cec4e36efb
Shane Jeremich <shane.jeremich@gmail.com>
2021-12-17 15:35:47 -0700

    changed shane jeremich text to button

--- src/layout/Nav/Nav.jsx
+++ src/layout/Nav/Nav.jsx
@@ -4,20 +4,22 @@ import "./Nav.scss";
 const Nav = () => {
   return (
     <nav className="nav">
-      <span className="header-title">
-        <button>SJ</button>
-      </span>
-      <span>
-        <Link className="header-link" to="#work">
-          Work
-        </Link>
-        <Link className="header-link" to="#portfolio">
-          Portfolio
-        </Link>
-        <Link className="header-link" to="#contact">
-          Contact
-        </Link>
-      </span>
+      <div className="wrapper">
+        <span className="header-title">
+          <button>SJ</button>
+        </span>
+        <span>
+          <Link className="header-link" to="#work">
+            Work
+          </Link>
+          <Link className="header-link" to="#portfolio">
+            Portfolio
+          </Link>
+          <Link className="header-link" to="#contact">
+            Contact
+          </Link>
+        </span>
+      </div>
     </nav>
   );
 };
