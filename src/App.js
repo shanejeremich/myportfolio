eb72a1bdb4f5ee028efdde5a463a7effec0b4eb7
Shane Jeremich <shane.jeremich@gmail.com>
2022-01-25 11:08:36 -0700

    layout: Updated theme
    added black background to the hero image, have the rows starting point set up now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Home from "./pages/Home/Home";
 import Nav from "./layout/Nav/Nav";
 import HeroImage from "./layout/HeroImage/HeroImage";
+import Home from "./pages/Home/Home";
+import Footer from "./layout/Footer/Footer";
 
 import "./sass/style.scss";
 
@@ -8,8 +9,12 @@ function App() {
   return (
     <div className="container">
       <Nav />
+      <div className="hero-bg"></div>
       <HeroImage />
+      <div className="row-1"></div>
       <Home />
+      <div className="row-2"></div>
+      <Footer />
     </div>
   );
 }
---
+++ src/layout/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import "./Footer.scss";
+
+function Footer() {
+  return <div className="footer">Footer</div>;
+}
+
+export default Footer;
