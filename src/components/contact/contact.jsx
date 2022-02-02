fdaa142bad796c5e2b42ec8ea9ade04a3a53e6f6
Shane Jeremich <shane.jeremich@gmail.com>
2022-02-02 14:17:48 -0700

    Layout: added reCaptcha
    added liveformhq api to contact form, added google reCaptcha to form

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,6 +1,16 @@
 import "./contact.scss";
 
 function Contact() {
+  const url = `https://liveformhq.com/form/0e09ca88-85f3-4860-bad7-b7fb2ee5eeba`;
+  const key = `6Ld7yVMeAAAAAMmURG1v-0SSc8a8mTCgHPTf2yvs`;
+
+  window.onload = function () {
+    var el = document.getElementById("g-recaptcha-response");
+    if (el) {
+      el.setAttribute("required", "required");
+    }
+  };
+
   return (
     <section className="contact" id="contact">
       <div className="contact__card">
@@ -12,11 +22,15 @@ function Contact() {
             <h1>Lets get together and create some stuff</h1>
             <h2>Message me your ideas and I'll get back to you right away</h2>
           </header>
-          <form className="contact__form" method="post" action="#">
-            <input type="text" name="name" placeholder="Name" className="contact__form--name" />
-            <input type="email" name="email" placeholder="Email" className="contact__form--email" />
-            <input type="text" name="subject" placeholder="Subject" className="contact__form--subject" />
-            <textarea type="text" name="message" placeholder="Message" className="contact__form--message" />
+          <form className="contact__form" method="post" action={url} acceptCharset="utf-8">
+            <input type="hidden" name="utf8" value="✓" />
+            <input type="hidden" name="_redirect" value="https://liveformhq.com/thank_you" />
+
+            <input type="text" name="name" placeholder="Name" className="contact__form--name" required />
+            <input type="email" name="email" placeholder="Email" className="contact__form--email" required />
+            <input type="text" name="subject" placeholder="Subject" className="contact__form--subject" required />
+            <textarea type="text" name="message" placeholder="Message" className="contact__form--message" required />
+            <div className="g-recaptcha" data-sitekey={key}></div>
 
             <div className="actions">
               <button type="submit">Send Message</button>
