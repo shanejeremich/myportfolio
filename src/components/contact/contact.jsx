5b035e9e1e2f5af83268194ec7002182bcda8168
Shane Jeremich <shane.jeremich@gmail.com>
2022-02-02 10:58:33 -0700

    Layout: Finished styling contact form
    finished adding the style for the contact form and restyled the main buton compponent.

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -4,10 +4,26 @@ function Contact() {
   return (
     <section className="contact" id="contact">
       <div className="contact__card">
-        <div className="contact__header text-bg--white">
+        <div className="contact__header text-bg--primary">
           <span>Contact Me</span>
         </div>
-        <div className="contact__card-content">card-content</div>
+        <div className="contact__card-content">
+          <header>
+            <h1>Lets get together and create some stuff</h1>
+            <h2>Message me your ideas and I'll get back to you right away</h2>
+          </header>
+          <form className="contact__form" method="post" action="#">
+            <input type="text" name="name" placeholder="Name" className="contact__form--name" />
+            <input type="email" name="email" placeholder="Email" className="contact__form--email" />
+            <input type="text" name="subject" placeholder="Subject" className="contact__form--subject" />
+            <textarea type="text" name="message" placeholder="Message" className="contact__form--message" />
+
+            <div className="actions">
+              <button type="submit">Send Message</button>
+              <button type="reset">Clear Form</button>
+            </div>
+          </form>
+        </div>
       </div>
     </section>
   );
