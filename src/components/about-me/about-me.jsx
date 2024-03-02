215eea4012273e4413d2277cd90d048165139145
shanejeremich <shane.jeremich@gmail.com>
2024-03-02 00:08:26 -0700

    feat(html): update profile picture in about me component
    Added a new image and replaced the profile picture in the about me component.

--- src/components/about-me/about-me.jsx
+++ src/components/about-me/about-me.jsx
@@ -1,4 +1,4 @@
-import userPic from "../../images/profile/profile.webp";
+import userPic from "../../images/profile/portfolio.webp";
 import "./about-me.scss";
 
 const AboutMe = () => {
@@ -16,24 +16,21 @@ const AboutMe = () => {
         <span className="bar" aria-hidden="true">
           &nbsp;&nbsp;
         </span>
-        As an ambitious web developer, I bring a problem-solving approach and a strong technology
-        skill set to the web development process. With a solid foundation from Bloom Institute of
-        Technology and a continued pursuit of a Bachelor of Science Degree in Web Development at
-        Full Sail University, I am passionate about contributing to the success of dynamic teams.
+        As an ambitious web developer, I bring a problem-solving approach and a strong technology skill set to the web development process. With a solid
+        foundation from Bloom Institute of Technology and a continued pursuit of a Bachelor of Science Degree in Web Development at Full Sail University, I am
+        passionate about contributing to the success of dynamic teams.
       </p>
 
       <p className="text-content">
-        During my certification program, I spearheaded diverse projects, collaborating with multiple
-        teams, and working on everything from front-end to back-end development. I have a proven
-        track record of effectively communicating with teams to understand web marketing needs and
-        crafting interfaces that precisely meet specifications.
+        During my certification program, I spearheaded diverse projects, collaborating with multiple teams, and working on everything from front-end to back-end
+        development. I have a proven track record of effectively communicating with teams to understand web marketing needs and crafting interfaces that
+        precisely meet specifications.
       </p>
 
       <p className="text-content">
-        I am currently seeking opportunities as an intern, apprentice, or junior web developer, with
-        a focus on front-end or full-stack development. I am excited to discuss how I can bring my
-        skills and enthusiasm to contribute to your company's success. Please feel free to reach out
-        to me for further discussions.
+        I am currently seeking opportunities as an intern, apprentice, or junior web developer, with a focus on front-end or full-stack development. I am
+        excited to discuss how I can bring my skills and enthusiasm to contribute to your company's success. Please feel free to reach out to me for further
+        discussions.
       </p>
 
       <a href="#skills">
